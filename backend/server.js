87f158632ab8353c76befd9d512d44ed7f9e4134
MD. TASNIM FERDOUS <105316843+TFS-here@users.noreply.github.com>
2024-12-10 14:19:02 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,19 +16,7 @@ connectCloudinary()
 
 // Middlewares
 app.use(express.json())
-app.use(
-     cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed methods
-    credentials: true, // Allow cookies if needed
-  })
-)
+app.use(cors())
 
 
 //API endpoints
